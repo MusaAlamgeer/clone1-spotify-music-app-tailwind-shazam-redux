72868db57d0f0a4f0a95a6913be849150e1faeea
musaa <musaa@LAPTOP-RJ6UHQ4R>
2022-10-01 01:29:39 +0500

    API Song Fetch in Redux, Loading, Error Component

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+// Service method is used to store the API Call and API Required Parameters
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '9cc147be0emshe4494d7a751e32ap105d81jsn0090dd33924b',
+//     'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
+//   },
+// };
+
+// fetch('https://shazam-core.p.rapidapi.com/v1/search/multi?offset=3&query=Masked&search_type=SONGS_ARTISTS', options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '9cc147be0emshe4494d7a751e32ap105d81jsn0090dd33924b');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
